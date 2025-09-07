c0029d7abc4e4d1600412a49ef63556adb124271
Vlad <vlad.kuzmenko.dev@gmail.com>
2025-09-08 00:20:43 +0300

    fix cookies

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,19 +11,18 @@ export const setupSession = (res, session) => {
     expires: session.refreshTokenValidUntil,
   });
 
-  res.cookie("sessionId", session._id.toString(), {
+  res.cookie("sessionId", session.userId, {
     httpOnly: true,
     expires: session.refreshTokenValidUntil,
   });
 };
 
 export const registerController = async (req, res) => {
-  const user = await registerUser(req.body);
+  await registerUser(req.body);
 
   res.status(201).json({
     status: 201,
     message: "Successfully registered user",
-    data: user,
   });
 };
 
@@ -34,7 +33,6 @@ export const loginController = async (req, res) => {
 
   res.json({
     status: 200,
-    message: "Successfully logged in an user!",
     data: {
       accessToken: session.accessToken,
     },
