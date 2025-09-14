f98bc7a6f7029df1603a93db7676d251a9851719
Vlad <vlad.kuzmenko.dev@gmail.com>
2025-09-14 21:45:20 +0300

    fix '400' error

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,7 +5,7 @@ export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
 
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(404, `${contactId} is not valid id`);
+    throw createHttpError(400, `${contactId} is not valid id`);
   }
   next();
 };
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,12 +3,10 @@ import createHttpError from "http-errors";
 export const validateBody = (schema) => {
   const middleware = async (req, res, next) => {
     try {
-      await schema.validateAsync(req.body, {
-        abortEarly: false,
-      });
+      await schema.validateAsync(req.body, { abortEarly: false });
       next();
     } catch (error) {
-      throw createHttpError(400, error.message);
+      next(createHttpError(400, error.message));
     }
   };
   return middleware;
